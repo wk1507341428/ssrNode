c3bdde670c4dc0e2083927ac90610af26c569676
路明非 <wk1507341428@gmail.com>
2019-08-14 17:50:21 +0800

    add

--- pages/rem.vue
+++ pages/rem.vue
@@ -1,8 +1,23 @@
 <template>
     <div class="container">
         <div class="box"></div>
+        <lessonformless></lessonformless>
     </div>
 </template>
+
+<script>
+import lessonformless from 'lesson-form'
+export default {
+    data(){
+        return{
+
+        }
+    },
+    components:{
+        lessonformless
+    }
+}
+</script>
 
 <style lang="scss">
 .container{
